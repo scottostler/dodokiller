36ae70143aa856ec3bc3e8dabf5aa2fad08f25a6
Toby Schachman <tobyschachman@gmail.com>
2011-01-29 13:09:04 -0500

    committing prototype, game logic documentation

---
+++ prototype/gamelogic.js
@@ -0,0 +1,31 @@
+/*
+
+game parameters
+  game speed
+  player rotate speed
+  player move speed
+  bullet move speed
+  bullet travel distance
+
+
+game state consists of
+  players
+    facing (angle)
+    position
+      x
+      y
+  bullets
+    facing (angle)
+    position
+      x
+      y
+    distance (how far it's travelled)
+  dodos
+    position
+      x
+      y
+  obstruction
+    function to determine whether a pixel is on or off limits
+
+*/
+
