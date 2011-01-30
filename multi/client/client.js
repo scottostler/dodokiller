c9a7324aa8da1679f90f06d8c2df9b7b4fa87ba0
Toby Schachman <tobyschachman@gmail.com>
2011-01-29 23:03:06 -0500

    new protocol receiving on client, tested locally

--- multi/client/client.js
+++ multi/client/client.js
@@ -32,40 +32,58 @@ sprites (server to client)
 
 */
 
+var sprites = {};
+
 function receiveFromServer(msg) {
   var data = msg.data;
   if (Math.random() > 0.99) { console.log(msg); }
-    
-  // msg is a series of sprites, need to draw them.
   
-  // clear the canvas
-  $("#canvas").html("");
-  
-  $.each(data, function (i, sprite) {
-    if (sprite.type == "player") {
-      var s = makeSprite(40, 60, "../../media/hat_new_2.png");
-      s.draw(sprite.x, sprite.y, Math.round((sprite.facing / (Math.PI*2))*36) % 36);
-    } else if (sprite.type == "dodo") {
-      var s = makeSprite(40, 40, "../../media/dodo.png");
-      s.draw(sprite.x, sprite.y, 0);
-    } else if (sprite.type == "bullet") {
-      var s = makeSprite(6, 6, "../../media/bullet.png");
-      s.draw(sprite.x, sprite.y, 0);
+  $.each(data, function (i, event) {
+    if (event.event === "create") {
+      var s;
+      if (event.type == "player") {
+        s = makeSprite(40, 60, "../../media/hat_new_2.png");
+      } else if (event.type == "dodo") {
+        s = makeSprite(40, 40, "../../media/dodo.png");
+      } else if (event.type == "bullet") {
+        s = makeSprite(6, 6, "../../media/bullet.png");
+      }
+      s.draw(event.x, event.y, event.facing || 0);
+      sprites[event.id] = s;
+    } else if (event.event === "update") {
+      sprites[event.id].draw(event.x, event.y, event.facing || 0);
+    } else if (event.event === "destroy") {
+      sprites[event.id].destroy();
+      delete sprites[event.id];
     }
   });
 }
 
 
 // for testing
-var sampleMessageFromServer = [
-  {"type": "player", "x": 200, "y": 200, "facing": 0},
-  {"type": "player", "x": 400, "y": 400, "facing": 1.2345},
-  {"type": "dodo", "x": 30, "y": 180},
-  {"type": "dodo", "x": 280, "y": 300},
-  {"type": "bullet", "x": 220, "y": 200},
-  {"type": "bullet", "x": 240, "y": 200}
+var sampleMessages = [
+  [
+    {"event": "create", "id": 0, "type": "player", "x": 200, "y": 200, "facing": 0},
+    {"event": "create", "id": 1, "type": "player", "x": 400, "y": 400, "facing": 1.2345},
+    {"event": "create", "id": 2, "type": "dodo", "x": 30, "y": 180},
+    {"event": "create", "id": 3, "type": "dodo", "x": 280, "y": 300},
+    {"event": "create", "id": 4, "type": "bullet", "x": 220, "y": 200},
+    {"event": "create", "id": 5, "type": "bullet", "x": 240, "y": 200}
+  ],
+  [
+    {"event": "update", "id": 4, "x": 225, "y": 200},
+    {"event": "update", "id": 5, "x": 245, "y": 200}
+  ],
+  [
+    {"event": "destroy", "id": 2},
+    {"event": "destroy", "id": 3}
+  ],
+  [
+    {"event": "update", "id": 0, "x": 200, "y": 200, "facing": 0.1}
+  ]
+  
 ];
-// receiveFromServer(sampleMessageFromServer)
+// receiveFromServer({"data":sampleMessages[0]})
 
 
 var keyCodes = {
