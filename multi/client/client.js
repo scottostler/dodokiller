a9a0e52ee97511c30365201109abc13ca8ed145f
Toby Schachman <tobyschachman@gmail.com>
2011-01-29 19:03:36 -0500

    finished client code, just have to hook it up with websockets...

--- multi/client/client.js
+++ multi/client/client.js
@@ -24,9 +24,36 @@ function sendToServer(msg) {
 }
 
 function receiveFromServer(msg) {
+  // msg is a series of sprites, need to draw them.
   
+  // clear the canvas
+  $("#canvas").html("");
+  
+  $.each(msg, function (i, sprite) {
+    if (sprite.type == "player") {
+      var s = makeSprite(40, 60, "../../media/hat_new_2.png");
+      s.draw(sprite.x, sprite.y, Math.round((sprite.facing / (Math.PI*2))*36) % 36);
+    } else if (sprite.type == "dodo") {
+      var s = makeSprite(40, 40, "../../media/dodo.png");
+      s.draw(sprite.x, sprite.y, 0);
+    } else if (sprite.type == "bullet") {
+      var s = makeSprite(6, 6, "../../media/bullet.png");
+      s.draw(sprite.x, sprite.y, 0);
+    }
+  });
 }
 
+
+// for testing
+var sampleMessageFromServer = [
+  {"type": "player", "x": 200, "y": 200, "facing": 0},
+  {"type": "player", "x": 400, "y": 400, "facing": 1.2345},
+  {"type": "dodo", "x": 30, "y": 180},
+  {"type": "dodo", "x": 280, "y": 300},
+  {"type": "bullet", "x": 220, "y": 200},
+  {"type": "bullet", "x": 240, "y": 200}
+];
+// receiveFromServer(sampleMessageFromServer)
 
 
 var keyCodes = {
@@ -37,20 +64,32 @@ var keyCodes = {
   "32": "shoot"
 };
 
+function sendKey(cmd, down) {
+  if (cmd) {
+    sendToServer({
+      "cmd": cmd,
+      "down": down
+    });
+  }
+}
+
+var keyboardState = {};
 function keyboardInit() {
   $(document).keydown(function (e) {
-    var cmd = keyCodes[e.keyCode];
-    if (cmd) sendToServer({
-      "cmd": cmd,
-      "down": true
-    });
+    var code = e.keyCode;
+    if (!keyboardState[code]) {
+      keyboardState[code] = true;
+      cmd = keyCodes[e.keyCode];
+      sendKey(cmd, true);
+    }
   });
   $(document).keyup(function (e) {
-    var cmd = keyCodes[e.keyCode];
-    if (cmd) sendToServer({
-      "cmd": cmd,
-      "down": false
-    });
+    var code = e.keyCode;
+    if (keyboardState[code]) {
+      keyboardState[code] = false;
+      cmd = keyCodes[e.keyCode];
+      sendKey(cmd, false);
+    }
   });
 }
 $(keyboardInit);
